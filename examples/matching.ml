fb40c295ad7d14f86a6a409a877fe5367df3c6b0
Scott Smith <scott@cs.jhu.edu>
2020-09-23 11:27:36 -0400

    example of matching

---
+++ examples/matching.ml
@@ -0,0 +1,68 @@
+open Core
+
+(* Matching brackets exercise, see 
+   https://github.com/exercism/ocaml/tree/master/exercises/matching-brackets *)
+
+
+(* Mutable stack version derived from 
+   https://exercism.io/tracks/ocaml/exercises/matching-brackets/solutions/df9c50d0863147a39fd2f69222aa1b56
+
+   Used OCaml Stack library with exceptions, re-wrote to use Core
+
+*)    
+
+let are_balanced_stack s =
+  let stack_of_lefts = Stack.create() in
+  let match_with s c = Option.value_map (Stack.pop s) ~f: (fun c' -> Char.(=) c c') ~default: false in
+  let parse = function
+    |'(' |'{' |'[' as c -> Fn.const true @@ Stack.push stack_of_lefts c
+    |')' -> match_with stack_of_lefts '('
+    |'}' -> match_with stack_of_lefts '{'
+    |']' -> match_with stack_of_lefts '['
+    | _ -> true
+  in  
+  let r = String.fold ~init:true ~f:(fun b -> fun c -> b && parse c) s in
+    r && Stack.is_empty stack_of_lefts
+
+(* Another version which uses an exception for the empty stack pop case.  
+  The above version is arguably better since there is a good default to return for empty-pop *)
+
+let are_balanced_exn s =
+  let stack_of_lefts = Stack.create() in
+  let match_with s c = Char.(=) c (Stack.pop_exn s) in
+  let parse = function
+    |'(' |'{' |'[' as c -> Fn.const true @@ Stack.push stack_of_lefts c
+    |')' -> match_with stack_of_lefts '('
+    |'}' -> match_with stack_of_lefts '{'
+    |']' -> match_with stack_of_lefts '['
+    | _ -> true
+  in  
+  try let r = String.fold ~init:true ~f:(fun b -> fun c -> b && parse c) s in
+    r && Stack.is_empty stack_of_lefts
+  with _ -> false (* return false if any exception is raised (a bit of a sledgehammer) *)
+
+
+
+
+(* No, you don't really need mutation here at all, and the code is shorter to boot.
+   Solution derived from 
+   https://exercism.io/tracks/ocaml/exercises/matching-brackets/solutions/ac5921390cb14120b44f049ef1a09186 
+*)
+
+let are_balanced_functional s =
+  String.fold_until s ~init:[]
+    ~f:(fun acc c -> match c, acc with
+        | '(', _ | '[', _ | '{', _ -> Continue (c :: acc)
+        | ')', ('('::tl) | ']', ('['::tl) | '}', ('{'::tl) -> Continue tl
+        | ')', _ | ']', _ | '}', _ -> Stop false
+        | _ -> Continue acc)
+    ~finish:List.is_empty
+
+(* Here is a simpler example of fold_until to better understand the above *)
+
+let summate_til_zero s =
+  List.fold_until s ~init:0
+    ~f:(fun acc i -> match i, acc with
+        | 0, sum -> Stop sum
+        | _, sum -> Continue (i + sum))
+    ~finish:Fn.id
