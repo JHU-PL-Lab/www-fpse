ca62c3c039927239581ff01cd6d7e8b16f745f59
Scott Smith <scott@cs.jhu.edu>
2020-09-30 12:45:43 -0400

    forgot a file

--- examples/school.ml
+++ examples/school.ml
@@ -7,10 +7,10 @@ open Core
 
 module IntMap = Map.Make(Int)
 
-(* The Int here is the type of keys of the map; we need to use a functor because we need 
-   an underling compare function for maps to work.  Built-in Core.Int has such. 
+(* The Int here is the **keys** of the map (the grade here); we need to use a functor because 
+  we need an underling compare function for maps to work.  Built-in Core.Int has such. 
    
-Here in fact is the module type of Make's argument:
+Here is the module type of Make's argument:
 
 module type Key =
   sig
@@ -20,15 +20,20 @@ module type Key =
     val sexp_of_t : t -> Sexp.t
   end
   
-  So it needs to have the underlying type t (int here), compare, plus to-from s-expression 
+  So Int needs to have the underlying type t (= int here), compare, plus to-from s-expression 
   functions.
 
   *)
 
 (* We are defining the School module; let us follow convention and name 
   "its" underlying data type t.  
-  Note that IntMap has one type parameter which is string list for a School *)
+  Note that IntMap has one type parameter which is the map's range
+   -- string list for a School *)
 type t = (string list) IntMap.t
+
+(* The empty school *)
+
+let empty = IntMap.empty
 
 (**  Add a student stud in grade grade to school database 
      Map.add_multi assumes values are lists and conses to key's list
@@ -41,7 +46,7 @@ let add grade stud (school : t) =  IntMap.add_multi school ~key:grade ~data:(stu
 **)
 let sort (school : t) = 
   IntMap.fold school
-    ~init:IntMap.empty 
+    ~init:empty 
     ~f:(fun ~key -> fun ~data -> fun scl -> IntMap.add_exn scl ~key ~data:(List.sort data ~compare:(String.compare) ))
 
 (** Note that Map.map is a better way; it maps over the values only, keeping key structure intact **)
@@ -51,6 +56,6 @@ let sort_better_with_map (school : t) =
 
 let roster school = school |> sort |> IntMap.data |> List.concat
 
-let dump school = school |> IntMap.to_alist
+let dump school = school |> IntMap.to_alist (* Auxiliary function to dump data structure *)
 
-let test = IntMap.empty |> add 2 "Ku" |> add 3 "Lu" |> add 9 "Mu" |> add 9 "Pupu"
+let test_school = empty |> add 2 "Ku" |> add 3 "Lu" |> add 9 "Mu" |> add 9 "Pupu"  |> add 9 "Apu"
