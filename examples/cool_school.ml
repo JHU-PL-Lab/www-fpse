7ef28d7c71e4a0864fcce49241c8bf051e554fa4
Scott Smith <scott@cs.jhu.edu>
2021-09-29 13:11:04 -0400

    minor

--- examples/cool_school.ml
+++ examples/cool_school.ml
@@ -4,7 +4,6 @@
 
 open Core
 
-let empty = Map.empty(module Int) (* new syntax: first-class module Int *)
 
 (* Map.t has three type parameters: key type, value type, 
    and a nonce (witness) for the comparison function used for the key.
@@ -13,7 +12,7 @@ let empty = Map.empty(module Int) (* new syntax: first-class module Int *)
 
 type t = (int, string list, Int.comparator_witness) Map.t
 
-
+let (empty : t) = Map.empty(module Int) (* new syntax: first-class module Int *)
 
 (* ***************************************************** *)
 (* Everything from here on down is the same as school.ml *)
@@ -25,24 +24,24 @@ type t = (int, string list, Int.comparator_witness) Map.t
 (**  Add a student stud in grade grade to school database 
      Map.add_multi assumes values are lists and conses to key's list
      or, creates a new key and singleton list if key not present. **)
-let add grade stud (school : t) =  Map.add_multi school ~key:grade ~data:(stud)
+let add grade stud (school : t) =  Map.add_multi school ~key:grade ~data:stud
 
 (** 
   Sorting using a fold over the map.
   Folding over a map is like folding over a list but you get both key and value
 **)
-let sort (school : t) = 
+let sort (school : t) : t = 
   Map.fold school
     ~init:empty 
     ~f:(fun ~key -> fun ~data -> fun scl -> Map.add_exn scl ~key ~data:(List.sort data ~compare:(String.compare) ))
 
 (** Note that Map.map is a better way; it maps over the values only, keeping key structure intact **)
-let sort_better_with_map (school : t) = 
+let sort_better_with_map (school : t) : t = 
   Map.map school
     ~f:(fun data -> (List.sort data ~compare:(String.compare) ))
 
-let roster school = school |> sort |> Map.data |> List.concat
+let roster (school : t) = school |> sort |> Map.data |> List.concat
 
-let dump school = school |> Map.to_alist
+let dump (school : t) = school |> Map.to_alist
 
 let test = empty |> add 2 "Ku" |> add 3 "Lu" |> add 9 "Mu" |> add 9 "Pupu"
