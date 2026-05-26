16f357d16730bfe0c661b1d68b83ad1e100022df
Scott Smith <scott@cs.jhu.edu>
2026-05-26 15:12:01 -0400

    no message

--- js/init.js
+++ js/init.js
@@ -6,42 +6,7 @@
 
 (function($) {
 
-	skel.init({
-		reset: 'full',
-		breakpoints: {
-			global:		{ range: '*', href: '/fpse/css/style.css', containers: 1400, grid: { gutters: 50 } },
-			wide:		{ range: '-1680', href: '/fpse/css/style-wide.css', containers: 1200, grid: { gutters: 40 } },
-			normal:		{ range: '-1280', href: '/fpse/css/style-normal.css', containers: 960, lockViewport: true },
-			narrow:		{ range: '-980', href: '/fpse/css/style-narrow.css', containers: '95%', grid: { gutters: 25 } },
-			narrower:	{ range: '-840', href: '/fpse/css/style-narrower.css', grid: { collapse: true } },
-			mobile:		{ range: '-640', href: '/fpse/css/style-mobile.css', containers: '90%', grid: { gutters: 15 } }
-		}
-	}, {
-		layers: {
-			layers: {
-				navPanel: {
-					animation: 'pushX',
-					breakpoints: 'narrower',
-					clickToClose: true,
-					height: '100%',
-					hidden: true,
-					html: '<div data-action="navList" data-args="nav"></div>',
-					orientation: 'vertical',
-					position: 'top-left',
-					side: 'left',
-					width: 275
-				},
-				titleBar: {
-					breakpoints: 'narrower',
-					height: 44,
-					html: '<span class="toggle" data-action="toggleLayer" data-args="navPanel"></span><span class="title" data-action="copyHTML" data-args="logo"></span>',
-					position: 'top-left',
-					side: 'top',
-					width: '100%'
-				}
-			}
-		}
-	});
+	
 
 	$(function() {
 
