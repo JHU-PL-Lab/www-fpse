477b221e5e64880c99ec246ac532bd1443e70484
Brandon Stride <brandonzstride@hotmail.com>
2026-07-27 10:55:53 -0400

    module-dependent functions in advanced modules

--- lecture/advanced_modules.ml
+++ lecture/advanced_modules.ml
@@ -2,9 +2,6 @@
 (* More on types, modules, functors and hiding *)
 (* ******************************************* *)
 
-(* Will need #require "ppx_deriving.std" in top loop
-   and (preprocess (pps ppx_deriving.std) line in dune for this file to work *)
-
 (* Review: the Big Picture of what is unique in OCaml types
 
   * One of the most novel/powerful/elegant/frustrating/confusing aspects of OCaml is the types
@@ -24,7 +21,7 @@ let f (type a) (x : a) = x;;
 let rl = ref [];;
 (* return val rl : '_weak1 list ref = {contents = []} in top loop *)
 
-(* '_weak1 etc are unknown types which are *not* polymorphic, 
+(* '_weak1 etc are unknown types which are *not* polymorphic,
    they can be only *one* thing, but currently that type is not known.
    For this example, its clear why it is not known, we only have an empty list.
    And, as discussed earlier it can't be polymorphic, whatever we put in must come out *)
@@ -37,8 +34,7 @@ let _ = !rl (* we have set '_weak1 to be int with this *)
 let weak_id = (fun () -> Fun.id) () (* abstraction and application are a no-op but messes up type *)
 (* val weak_id : '_weak2 -> '_weak2 = <fun> *)
 
-(*
- * Type variables can be PARAMETERS on type - to - type functions *)
+(* Type variables can be PARAMETERS on type - to - type functions *)
 
 type 'a my_option = My_some of 'a | My_none
 
@@ -93,15 +89,14 @@ type ('key, 'value, 'cmp) t = ('key, 'value, 'cmp) Map.t (* existential / hidden
 
 (* An abstract pairing module type: *)
 
-module type PAIR = 
- sig
-   type l
-   type r
-   type t = l * r
-   val create : l -> r -> t
-   val left : t -> l
-   val right : t -> r
-   val equal : t -> t -> bool
+module type PAIR = sig
+  type l
+  type r
+  type t = l * r
+  val create : l -> r -> t
+  val left : t -> l
+  val right : t -> r
+  val equal : t -> t -> bool
 end
 
 (* Arbitrary Data with equality module type *)
@@ -110,17 +105,18 @@ module type DATUM = sig
   val equal : t -> t -> bool
 end
 
-(* Functor to make a pair of two Datum's, 
+(* Functor to make a pair of two Datum's,
   and the "with :=" syntax  will substitute those types in the result module type. *)
-module Make_pair_smartest(Datum1 : DATUM)(Datum2 : DATUM) : (PAIR with type l := Datum1.t with type r := Datum2.t) = 
-struct
+module Make_pair_smartest(Datum1 : DATUM)(Datum2 : DATUM)
+  : (PAIR with type l := Datum1.t with type r := Datum2.t) = struct
   type l = Datum1.t
   type r = Datum2.t
   type t = l * r
   let create (l : l) (r : r) : t = (l,r)
   let left (p : t) = match p with (a,_) -> a
   let right (p : t) = match p with (_,b) -> b
-  let equal (p1 : t) (p2 : t) = Datum1.equal (left p1) (left p2) && Datum2.equal (right p1) (right p2)
+  let equal (p1 : t) (p2 : t) =
+    Datum1.equal (left p1) (left p2) && Datum2.equal (right p1) (right p2)
 end
 module Example_pair_smartest = Make_pair_smartest(Int)(String)
 
@@ -129,15 +125,14 @@ module Example_pair_smartest = Make_pair_smartest(Int)(String)
 (* ******************* *)
 
 (* Treat modules as data values: let-define them, put in lists, etc etc *)
-(* We will see there are ways to push modules into expression-space, 
+(* We will see there are ways to push modules into expression-space,
    and also to take modules in expression-space and turn them into real modules *)
 (* We often need an explicit module type added (`: MYMODULE`) to get this to work *)
 (* (in general, the more advanced the types get the weaker the inference is) *)
 
 (* The module type for Example_pair_smartest above, declared module type needed explicitly *)
 (* This is the type inferred for the resulting module the functor makes *)
-module type EPS = 
-sig
+module type EPS = sig
   type t
   val create : int -> string -> t
   val left : t -> int
@@ -148,65 +143,98 @@ end
 
 (* Let us put a module in a ref cell as a very stupid example of modules-as-data
     `(module M)` is the syntax for turning regular module into an expression
-    But often the type checker needs some help so you need to also give a type:  
+    But often the type checker needs some help so you need to also give a type:
     `(module M : MOD)`  *)
 
-let mcell  =  ref (module Example_pair_smartest : EPS)
+(*
+  We turn a module into a first class value with the `(module M)` syntax.
+  Often the type checker needs some help, so we annotate it with a type like
+  `(module M : MODULE_TYPE)`.
+*)
+
+let mcell = (module Example_pair_smartest : EPS)
 
 (* To use it, first need to unpack it from the cell to make an official top-level module *)
 (* "val" is the keyword to go back expression-land to module-land
    - it is an inverse of (module ..) syntax *)
 
-module SP = (val !mcell)
+module SP = (val mcell)
 
-let _ : int = 
+let _ : int =
   let p = SP.create 4 "ho" in SP.left p
 
 (* Can also locally unpack it with `let (Module M)` syntax *)
-let _ : int = 
-  let (module M) = !mcell in let p = M.create 4 "ho" in M.left p;;
+let _ : int =
+  let (module M) = mcell in let p = M.create 4 "ho" in M.left p;;
 
 
 (* ************************************************************************** *)
 
-(* ************************************************************ *)
-(* First-class modules in the standard data structure libraries *)
-(* ************************************************************ *)
+(* When to use first class modules? Nowadays, they can be used to avoid single-
+  purpose functors--functors that derive a module with just one function. *)
 
-(* Warm-up with review of Map.Make
+(* We all love monads, no? *)
 
-   When we previously discussed Map etc we showed how to use Map.Make to make
-   maps over a certain type of data.
-
-   Here is the module type of Map.Make:
-
-   #show Map.Make;;
-   module Make :
-   functor (Ord : Map.OrderedType) -> sig (* .. tons of stuff *) end
-
-   So let us look at what the module type Map.OrderedType is:
-
-   #show Map.OrderedType;;
-   module type OrderedType = sig type t val compare : t -> t -> int end
-
-   - it needs `compare`.  One easy way to get that is with @@deriving ord:
-*)
-
-module IntPair = struct
-  type t = int * int [@@deriving ord]
+module type MONAD = sig
+  type 'a m
+  val return : 'a -> 'a m
+  val ( let* ) : 'a m -> ('a -> 'b m) -> 'b m
 end
 
-(* Note that defining compare yourself is also not very hard in such a simple case.. *)
-module IntPairAlt = struct
-  type t = int * int
-  let compare (x:t) (y:t) = compare x y
+(* If we want to define a list-fold with a monad, we usually would use a functor *)
+
+module Make_list_fold (M : MONAD) = struct
+  let list_fold_leftM f init_m ls =
+    let rec fold acc_m = function
+    | [] -> acc_m
+    | hd :: tl ->
+      let open M in
+      let* acc = acc_m in
+      fold (f acc hd) tl
+    in
+    fold init_m ls
 end
 
-module PairMap = Map.Make(IntPair)
-let pair_map = PairMap.empty
-let add_a_pair = pair_map |> PairMap.add (1,2) 3
+module Opt = struct
+  type 'a m = 'a option
+  let ( let* ) = Option.Syntax.( let* )
+  let return a = Some a
+end
 
-(* Now the somewhat annoying thing about this is we need to make a new module for every different Map key type *)
+(* To get a list_fold_leftM, we have to call the functor, name the resulting
+  module, and then access from that module. *)
+module Opt_fold = Make_list_fold (Opt)
+
+let sum_if_pos ls =
+  Opt_fold.list_fold_leftM (fun acc a ->
+    if a > 0 then
+      Some (acc + a)
+    else
+      None
+  ) (Some 0) ls
+
+(* But this is a lot of heavy machinery for a single function. Module-dependent
+  functions come to our rescue! *)
+
+let list_fold_leftM (module M : MONAD) f init_m ls =
+  let rec fold acc_m = function
+  | [] -> acc_m
+  | hd :: tl ->
+    let open M in
+    let* acc = acc_m in
+    fold (f acc hd) tl
+  in
+  fold init_m ls
+
+(* With module-dependent functions, we just pass the module on the fly! *)
+let sum_if_pos' ls =
+  list_fold_leftM (module Opt) (fun acc a ->
+    if a > 0 then
+      Some (acc + a)
+    else
+      None
+  ) (Some 0) ls
+
 
 (* **************** *)
 (* **** I/O ******* *)
@@ -217,13 +245,13 @@ let add_a_pair = pair_map |> PairMap.add (1,2) 3
 
 (* First, printf, sprintf, fprintf tend to "just work" so you don't necessarily need to know this
    But it can help if you are getting strange error messages to know it is complex under the hood *)
-   
+
 let () = Printf.printf "%i is the number\n" 5;;
 let () = Printf.printf "%i is the number and %s is the string \n" 5 "hoo";;
 
 (* The compiler is doing special things with the argument here, it is converting it into
    a function which will do this particular output taking 5 as a parameter
-   
+
    Why?? Printing is fully type-safe in OCaml, if you pass the wrong type of value
     you will get a type error ! *)
 
@@ -236,7 +264,7 @@ open CamlinternalFormatBasics (* shorten what is printed *)
 (* Lets give the string above a format type *)
 let fmt : (int -> 'a, 'b, 'c) format =  "%i is the number \n"
 
-(* observe the first parameter is a function taking an int - that is extracted from the %i 
+(* observe the first parameter is a function taking an int - that is extracted from the %i
   by the compiler.  Ignore the other parameters, they are for internal use.
   Note the function will be inferred if we leave it out. *)
 let fmt2 : ('a, 'b, 'c) format =  "%i is the number \n"
@@ -253,9 +281,9 @@ let () = print_int 5;;
 (* One more format example with multiple arguments *)
 let fmt3 : (int -> string -> string -> 'c, 'b, 'c) format =  "%i is the number %s is the string %s too \n";;
 
-let () = Printf.printf fmt3 4 "k" "l";; 
+let () = Printf.printf fmt3 4 "k" "l";;
 
-(* Note printf is Out_channel.printf and there is also 
-  - fprintf (print to any out_channel including network file etc; printf is (fprintf stdout)) 
-  - sprintf (just "print" onto a string), 
+(* Note printf is Out_channel.printf and there is also
+  - fprintf (print to any out_channel including network file etc; printf is (fprintf stdout))
+  - sprintf (just "print" onto a string),
   - eprintf (print to std error), etc *)
